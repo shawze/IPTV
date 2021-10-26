68eab1693bd4bffe249efd30e55618127f6d083a
xiaoze <xiaoze@live.com>
2021-10-26 23:04:53 +0800

    1. 合并securecrt的m3u格式转换python代码，修改输出文件到output目录; 2. 整合组播调试到一个xls文件; 3. 增加CCTV16奥运频道

--- conv2m3u.py
+++ conv2m3u.py
@@ -8,9 +8,11 @@
 from openpyxl import load_workbook
 import os
 
-udpxy_srv="http://10.0.0.1:4022/udp/"
+# udpxy_srv="http://10.0.0.1:4022/udp/"
+udpxy_srv="rtp://"
 
-def Conv_Xls2m3u(f_xls):
+
+def Conv_Xls2m3u(f_xls,debug=False):
     curpath = os.path.dirname(os.path.abspath(__file__))
     xlsx_file=os.path.join(curpath,f_xls)
     workbook = load_workbook(filename=xlsx_file,data_only=True)
@@ -18,6 +20,9 @@ def Conv_Xls2m3u(f_xls):
     sheets = workbook.sheetnames
     sheets_cnt = len(sheets)
     print(sheets)
+    output_path = os.path.join(curpath, "output")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     for s in sheets:
         st = workbook[s]
         print(st.title)
@@ -25,7 +30,8 @@ def Conv_Xls2m3u(f_xls):
             m3u_file = f_prefix+"-"+st.title+".m3u"
         else:
             m3u_file = f_prefix+".m3u"
-        h_m3u= open(m3u_file,"wt")
+        m3u_filepath = os.path.join(output_path,m3u_file)
+        h_m3u= open(m3u_filepath,"wt")
         start_idx=2
         # A2 B2 C2
         while True:
@@ -34,7 +40,7 @@ def Conv_Xls2m3u(f_xls):
             idxC = "C"+str(start_idx)
             title = st[idxA].value
             if title!=None:
-                if title == "#":
+                if title == "#" and not debug:
                     start_idx+=1
                     continue
                 igmp = str(st[idxB].value)
@@ -42,7 +48,7 @@ def Conv_Xls2m3u(f_xls):
                     print(igmp[0])
                     igmp = str(st[idxC].value)
                 print(title,igmp)
-                h_m3u.writelines("#EXTINF:-1, "+title+"\n")
+                h_m3u.writelines("#EXTINF:-1, "+str(title)+"\n")
                 udpxy_addr=igmp.replace("igmp://",udpxy_srv)
                 h_m3u.writelines(udpxy_addr+"\n")
                 start_idx+=1
@@ -50,6 +56,5 @@ def Conv_Xls2m3u(f_xls):
                 break
         h_m3u.close()
 
-Conv_Xls2m3u("江苏电信.xlsx")
-Conv_Xls2m3u("江苏电信-组播-标清.xlsx")
-Conv_Xls2m3u("江苏电信-组播-高清.xlsx")
+Conv_Xls2m3u("江苏电信-组播.xlsx")
+Conv_Xls2m3u("江苏电信-组播(调试).xlsx",debug=True)
